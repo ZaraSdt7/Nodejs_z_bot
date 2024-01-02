cdadd41aa297017174f6b2b6eef20b1e01fa4490
zahra <zahra.st7373@gmail.com>
2024-01-02 14:41:11 +0330

    update

--- middlware.js
+++ middlware.js
@@ -1,4 +1,11 @@
 const {Telegraf} = require("telegraf")
 require("dotenv").config();
-const  bot = new Telegraf(process.env.API_KEYS)
+const  bot = new Telegraf(process.env.API_KEYS);
+bot.use((ctx,next)=>{
+const msg = ctx.message.text;
+if(msg == "zara") return next();
+})
+bot.hears("zara",(ctx)=>{
+    ctx.reply("welcome zara")
+})
 bot.launch();
--- on-method.js
+++ on-method.js
@@ -1,4 +1,15 @@
 const {Telegraf} = require("telegraf")
 require("dotenv").config();
-const  bot = new Telegraf(process.env.API_KEYS)
+const  bot = new Telegraf(process.env.API_KEYS);
+bot.on("photo",ctx=>{
+    ctx.reply(ctx.chat.username,"عکس فرستادی شیطون!😋")
+})
+bot.on("voice",ctx=>{
+    ctx.reply(ctx.chat.username,"وویس فرستاد گوش کنین!😃")
+})
+bot.on("new_chat_members",ctx=>{
+    const username = ctx.message.new_chat_member.username?? ctx.message.new_chat_member.first_name
+    ctx.reply(ctx.chat.first_name , "خوش اومدی عزیزم")
+})
+
 bot.launch();

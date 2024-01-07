f0f38c848b1bed119990285bf9a640cad8baf243
zahra <zahra.st7373@gmail.com>
2024-01-07 20:05:59 +0330

    ..

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const {Telegraf} = require("telegraf")
 require("dotenv").config();
 const  bot = new Telegraf(process.env.API_KEYS)
 bot.command("start",ctx=>{
-    ctx.reply("سلام سلام خوش اومدی خوشگله...");
+    ctx.reply("سلام سلام خوش اومدی ...");
     ctx.telegram.sendMessage(ctx.chat.id,"اسمت چیه؟...")
 })
 bot.command("help",ctx=>{
-ctx.reply(":برای استفاده از راهنما به تنظیمات برو خوشگلم")
+ctx.reply(":برای استفاده از راهنما به تنظیمات برو ")
 });
 bot.command(["setting","tools"],ctx=>{
     ctx.reply("یک لیست از تنظیمات برات گذاشتم که راحتر گزینها رو انتخاب کنی:")
